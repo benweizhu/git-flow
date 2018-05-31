ad6d35059df9f2ac947e8d01a73d1f56063f86a7
benweizhu <benweizhu@gmail.com>
2018-05-31 18:56:46 +0800

    add git flow

--- public/index.js
+++ public/index.js
@@ -1,1 +1,8 @@
-console.log('hi')
+var gitgraph = new GitGraph({
+    template: "blackarrow",
+    reverseArrow: false,
+    orientation: "horizontal",
+    mode: "compact"
+});
+
+console.log('sdfsdfsfs')
